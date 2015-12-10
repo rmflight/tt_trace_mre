8f94dfa3e34eeac8910691b0614258e5b42b3a9d
Robert M Flight <r.flight79@gmail.com>
2015-12-10 10:43:25 -0500

    try another approach

--- tests/testthat/test-num_calls.R
+++ tests/testthat/test-num_calls.R
@@ -1,30 +1,9 @@
 context("number of calls")
 
-count_env <- new.env()
-
-count_env$f1 <- count_env$f2 <- 0
-
-trace(f1, tracer = quote(count_env$f1 <- count_env$f1 + 1), print = FALSE)
-trace(f2, tracer = quote(count_env$f2 <- count_env$f2 + 1), print = FALSE)
+tt_pos <- match("package:ttTraceMRE", search())
+trace(f1, tracer = quote(add_f1()), print = FALSE, where = as.environment(tt_pos))
 
 test_that("f1 FALSE", {
   f1()
-  expect_equal(1, count_env$f1)
-  expect_equal(0, count_env$f2)
-})
-
-count_env$f1 <- count_env$f2 <- 0
-
-test_that("f1 TRUE", {
-  f1(TRUE)
-  expect_equal(2, count_env$f1)
-  expect_equal(1, count_env$f2)
-})
-
-count_env$f1 <- count_env$f2 <- 0
-
-test_that("f2 works", {
-  f2()
-  expect_equal(1, count_env$f1)
-  expect_equal(1, count_env$f2)
+  expect_equal(1, ttTraceMRE:::count_env$f1)
 })
