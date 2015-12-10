353b01c5e2d2325b57de3013a4fe19d43d93fc7c
Stephanie Locke <stephanie.g.locke@gmail.com>
2015-12-10 18:10:37 +0000

    Used variable
    http://stackoverflow.com/questions/21687514/r-count-function-calls

--- tests/testthat/test-num_calls.R
+++ tests/testthat/test-num_calls.R
@@ -1,12 +1,10 @@
 context("number of calls")
 
 test_that("f1 FALSE", {
-  count_env <- new.env()
+  .count <- 0
 
-  count_env$f1 <- 0
-
-  trace(f1, tracer = quote(count_env$f1 <<- count_env$f1 + 1), print = FALSE)
+  trace(f1, tracer = function() {.count <<- .count +1}, print = FALSE)
 
   f1()
-  expect_equal(1, count_env$f1)
+  expect_equal(1, .count)
 })
