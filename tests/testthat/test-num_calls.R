581197eb478a6a00f5fd0e3c67f64ce1dc2ca1ad
Robert M Flight <r.flight79@gmail.com>
2015-12-10 10:01:11 -0500

    initial crack at testing this

--- tests/testthat/test-num_calls.R
+++ tests/testthat/test-num_calls.R
@@ -1,8 +1,30 @@
-context("num_calls")
+context("number of calls")
 
-## TODO: Rename context
-## TODO: Add more tests
+count_env <- new.env()
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+count_env$f1 <- count_env$f2 <- 0
+
+trace(f1, tracer = quote(count_env$f1 <- count_env$f1 + 1), print = FALSE)
+trace(f2, tracer = quote(count_env$f2 <- count_env$f2 + 1), print = FALSE)
+
+test_that("f1 FALSE", {
+  f1()
+  expect_equal(1, count_env$f1)
+  expect_equal(0, count_env$f2)
+})
+
+count_env$f1 <- count_env$f2 <- 0
+
+test_that("f1 TRUE", {
+  f1(TRUE)
+  expect_equal(2, count_env$f1)
+  expect_equal(1, count_env$f2)
+})
+
+count_env$f1 <- count_env$f2 <- 0
+
+test_that("f2 works", {
+  f2()
+  expect_equal(1, count_env$f1)
+  expect_equal(1, count_env$f2)
 })
