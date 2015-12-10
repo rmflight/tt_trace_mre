eb4f4dc63792e2e4f1b1ee56b530733df71f50e8
Robert M Flight <r.flight79@gmail.com>
2015-12-10 13:34:59 -0500

    removing unneeded code

--- R/simple_functions.R
+++ R/simple_functions.R
@@ -20,23 +20,3 @@ f1 <- function(use_f2 = FALSE){
 f2 <- function(){
   f1(use_f2 = FALSE)
 }
-
-f1_count <- 0
-
-init_f_count <- function(){
-  tt_pos <- match("package:ttTraceMRE", search())
-  assign("f1_count", 0, envir = as.environment(tt_pos))
-}
-
-
-#' add f1
-#'
-#' adds 1 to f1
-#'
-#' @export
-#' @return nothing
-add_f1 <- function(){
-  tt_pos <- match("package:ttTraceMRE", search())
-  f1_val <- get("f1_count", envir = as.environment(tt_pos))
-  assign("f1_count", f1_val + 1, pos = tt_pos)
-}
