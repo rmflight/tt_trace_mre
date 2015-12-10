e5c9eb4814492786470043363ba8a83354ae3df0
Robert M Flight <r.flight79@gmail.com>
2015-12-10 13:34:32 -0500

    putting in full set of tests

--- tests/testthat/test-num_calls.R
+++ tests/testthat/test-num_calls.R
@@ -1,10 +1,29 @@
 context("number of calls")
 
+.f1 <- .f2 <- 0
+
+trace(f1, tracer = function() {.f1 <<- .f1 + 1}, print = FALSE)
+trace(f2, tracer = function() {.f2 <<- .f2 + 1}, print = FALSE)
+
 test_that("f1 FALSE", {
-  .count <- 0
-
-  trace(f1, tracer = function() {.count <<- .count +1}, print = FALSE)
-
   f1()
-  expect_equal(1, .count)
+  expect_equal(1, .f1)
+  expect_equal(0, .f2)
+})
+
+.f1 <- .f2 <- 0
+
+test_that("f1 TRUE", {
+  f1(TRUE)
+
+  expect_equal(2, .f1)
+  expect_equal(1, .f2)
+})
+
+.f1 <- .f2 <- 0
+
+test_that("f2 works", {
+  f2()
+  expect_equal(1, .f1)
+  expect_equal(1, .f2)
 })
