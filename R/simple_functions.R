a907af69db956a0f4020ade50c9b1271c75052f6
Robert M Flight <r.flight79@gmail.com>
2015-12-10 10:30:16 -0500

    making the counter function part of the package

--- R/simple_functions.R
+++ R/simple_functions.R
@@ -20,3 +20,17 @@ f1 <- function(use_f2 = FALSE){
 f2 <- function(){
   f1(use_f2 = FALSE)
 }
+
+count_env <- new.env()
+count_env$f1 <- 0
+
+#' add f1
+#'
+#' adds 1 to f1
+#'
+#' @export
+#' @return nothing
+add_f1 <- function(){
+  f1_val <- get("f1", envir = ttTraceMRE:::count_env)
+  assign("f1", f1_val + 1, envir = ttTraceMRE:::count_env)
+}
